12cfa4e5533c7c127dd98d69ab4a55e9aad1815a
JWHan717 <cony717@shiftup.co.kr>
2023-12-04 06:07:43 +0900

    basic highlighting based on score input

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,25 @@
-document.getElementById("highlightButton").onclick = function() {
+const scoreInput = document.getElementById("score");
+
+document.getElementById("highlightButton").onclick = function () {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(
             tabs[0].id, {
-                message: "highlight text"
-            },
+            message: "highlight text",
+            score: scoreInput.value
+        },
+            (response) => {
+                console.log(response);
+            }
+        );
+    });
+};
+
+document.getElementById("undoButton").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(
+            tabs[0].id, {
+            message: "undo highlight"
+        },
             (response) => {
                 console.log(response);
             }
--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,9 +1,31 @@
-const highlighter = document.createElement("ethifact-highlighter");
-document.body.appendChild(highlighter);
+const color_red = "rgb(252,217,217)";
+const color_grey = "rgb(240,240,240)";
+const color_green = "rgb(217, 250, 220)";
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === "highlight text") {
-        alert("OK!");
-        highlighter.setAttribute();
+        const links = document.getElementsByTagName("a");
+
+        for (let i = 0; i < links.length; i++) {
+            if (links.item(i).childElementCount === 0) {
+                if (Number(request.score) >= 80) {
+                    links.item(i).style.backgroundColor = color_green;
+                } else if (Number(request.score) >= 50) {
+                    links.item(i).style.backgroundColor = color_grey;
+                } else {
+                    links.item(i).style.backgroundColor = color_red;
+                }
+            }
+        }
+    }
+
+    if (request.message === "undo highlight") {
+        const links = document.getElementsByTagName("a");
+
+        for (let i = 0; i < links.length; i++) {
+            if (links.item(i).childElementCount === 0) {
+                links.item(i).style.backgroundColor = "initial";
+            }
+        }
     }
 });
