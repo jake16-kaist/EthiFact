06085dcc55431d7dff61c305b4fd1ef88963548d
JWHan717 <cony717@shiftup.co.kr>
2023-12-06 09:05:52 +0900

    ready for demo

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,5 +1,5 @@
 const color_red = "rgb(252,217,217)";
-const color_grey = "rgb(240,240,240)";
+const color_grey = "rgb(230,230,230)";
 const color_green = "rgb(217, 250, 220)";
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -9,14 +9,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
         for (let i = 0; i < links.length; i++) {
             if (links.item(i).childElementCount === 0) {
-                // if (Number(request.score) >= 80) {
-                //     links.item(i).style.backgroundColor = color_green;
-                // } else if (Number(request.score) >= 50) {
-                //     links.item(i).style.backgroundColor = color_grey;
-                // } else {
-                //     links.item(i).style.backgroundColor = color_red;
-                // }
-
                 if (links.item(i).href.includes("youtube")) {
                     links.item(i).style.backgroundColor = color_red;
                 } else if (links.item(i).href.includes("gutenberg")) {
